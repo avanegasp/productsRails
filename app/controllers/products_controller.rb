9663b19bec9b22a9673c5c5d8175eb8182a62e30
avanegasp <arco527@gmail.com>
2018-01-17 18:06:02 -0500

    Adding bootstrap, jquery gems

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,27 @@
 class ProductsController < ApplicationController
+
+def index
+  @products = Product.all
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  private
+  def products_params
+    params.require(:product).permit(:name, :price)
+  end
 end
